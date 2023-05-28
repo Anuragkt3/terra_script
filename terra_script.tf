62606b84872b759b6116b42e5a15c94badb8150d
Anurag <anuragkt3@gmail.com>
2023-05-28 13:49:18 +0530

    till SG

---
+++ terra_script.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_key_pair" "IntegrationKey" {
+
+  # Name of the key which you want to use
+  key_name   = "Anurag_123"
+
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+# Creating a security group!
+resource "aws_security_group" "ISG" {
+
+  # Write description for the security group!
+  description = "Allow HTTP Inbound Traffic"
+  ingress {
+    description = "HTTP for webserver"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH for webserver"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "ouput from webserver"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Project1"
+  }
+}
